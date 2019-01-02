7cce6331530cebca6c98b1f6360dd0f9597d2a7b
wallbreaker2 <784942619@qq.com>
2019-01-02 22:40:14 +0800

    fix bug

--- PythonTest/PythonTest.py
+++ PythonTest/PythonTest.py
@@ -7,17 +7,21 @@ print("hello world\n");
 op=Dispatch("op.opsoft");
 print("op ver:",op.Ver());
 
-hwnd=op.FindWindow("CabinetWClass","6.1647");
+hwnd=op.FindWindow("","五子棋(Piskvork)");
 print(hwnd);
 if hwnd:
-	ret=op.BindWindow(hwnd,1,1,0,0);
+	ret=op.BindWindow(hwnd,1,0,0,0);
 	if ret:
 		print("bind ok.");
 		op.Sleep(1000);
 		op.Capture("screen.bmp");
 		x=0;y=0;
-		r,x,y=op.FindPic(0,0,800,600,"test.bmp",0.9);
+		
+		r,x,y=op.FindPic(0,0,800,600,"test.png",0.9);
 		print("x:",x,"y",y);
+		op.MoveTo(x,y);
+		op.Sleep(1000);
+		op.LeftClick();
 	else:
 		print("bind false.");
 else:
--- op/Bkdisplay.h
+++ op/Bkdisplay.h
@@ -9,30 +9,49 @@ class Bkdisplay
 public:
 	Bkdisplay();
 	~Bkdisplay();
+	//绑定
 	long Bind(HWND _hwnd, int mode);
+	//解绑
 	long UnBind();
+	//截图初始化
 	long cap_init();
+	//截图释放
 	long cap_release();
+	//截图
 	long cap_image();
+	//截图至文件
 	long capture(const std::wstring& file_name);
+	//图形定位
 	long FindPic(long x1, long y1, long x2, long y2, const std::wstring& files, double sim, long& x, long &y);
 private:
+	//截图窗口句柄
 	HWND _hwnd;
+	//截图模式
 	int _mode;
+	//屏幕像素指针
 	byte* _image_data;
+	//图像宽高
 	long _width, _height;
+	//设备句柄
 	HDC _hdc;
 	HDC _hmdc;
-	HDC _hhh;
+	//位图句柄
 	HBITMAP _hbmpscreen;
 	HBITMAP _holdbmp;
-	BITMAP           _bm;    //bmp图像效果好，非压缩；jpg压缩，便于网络传输	
+	//位图信息
+	BITMAP           _bm;
+	//bmp 文件头
 	BITMAPFILEHEADER _bfh = { 0 };
 	BITMAPINFOHEADER _bih = { 0 };//位图信息头
+	//截图线程指针
 	std::thread* _pthread;
+	//互斥量
 	std::mutex _mutex;
+	//截图标识
 	int _is_cap;
+	//图形匹配
 	ImageLoc _imageloc;
+	//截图线程函数
 	int cap_thread();
 };
 
