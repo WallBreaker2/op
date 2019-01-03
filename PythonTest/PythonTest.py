7bbc2a581c1aab41abb64c1f1d8fed0cf6dfdd22
wallbreaker2 <784942619@qq.com>
2019-01-03 12:21:47 +0800

    修改bind接口

--- PythonTest/PythonTest.py
+++ PythonTest/PythonTest.py
@@ -1,28 +1,30 @@
 
 from win32com.client import Dispatch
 
-
-print("hello world\n");
-
 op=Dispatch("op.opsoft");
+
 print("op ver:",op.Ver());
 
-hwnd=op.FindWindow("","五子棋(Piskvork)");
-print(hwnd);
+hwnd=op.FindWindow("Qt5QWindowIcon","QWidgetClassWindow");
+
 if hwnd:
-	ret=op.BindWindow(hwnd,1,0,0,0);
+	print(hwnd);
+	ret=op.BindWindow(hwnd,1,1,0,0);
 	if ret:
 		print("bind ok.");
 		op.Sleep(1000);
 		op.Capture("screen.bmp");
 		x=0;y=0;
 		
-		r,x,y=op.FindPic(0,0,800,600,"test.png",0.9);
+		r,x,y=op.FindPic(0,0,800,600,"test.bmp",0.9);
 		print("x:",x,"y",y);
-		op.MoveTo(x,y);
-		op.Sleep(1000);
-		op.LeftClick();
+		if r==1:
+			op.MoveTo(x,y);
+			op.Sleep(1000);
+			op.LeftClick();
+		else:
+			print("not find pic");
 	else:
 		print("bind false.");
 else:
-	print("no window.");
+	print("invalid window.");
--- op/BackGround.cpp
+++ op/BackGround.cpp
@@ -4,7 +4,7 @@
 
 Background::Background() :_hwnd(0),_is_bind(0)
 {
-	_display = _keypad = _mode = 0;
+	_mode = 0;
 }
 
 
@@ -12,16 +12,57 @@ Background::~Background()
 {
 }
 
-long Background::Bind(int hwnd, int display, int mouse, int keypad, int mode) {
+long Background::Bind(long hwnd, const wstring& sdisplay, const wstring& smouse, const wstring& skeypad, long mode) {
 	_hwnd = (HWND)hwnd;
 	long ret;
+	int display, mouse, keypad;
+	//check display
+	if (sdisplay == L"normal")
+		display = BACKTYPE::NORMAL;
+	else if (sdisplay == L"gdi")
+		display = BACKTYPE::GDI;
+	else if (sdisplay == L"DX")
+		display = BACKTYPE::DX;
+	else if (sdisplay == L"opengl")
+		display = BACKTYPE::OPENGL;
+	else {
+		setlog(L"error sdisplay=%s", sdisplay.c_str());
+		return 0;
+	}
+	//check mouse
+	if (smouse == L"normal")
+		mouse = BACKTYPE::NORMAL;
+	else if (sdisplay == L"windows")
+		mouse = BACKTYPE::WINDOWS;
+	else if (sdisplay == L"DX")
+		mouse = BACKTYPE::DX;
+	else if (sdisplay == L"opengl")
+		mouse = BACKTYPE::OPENGL;
+	else {
+		setlog(L"error smouse=%s", smouse.c_str());
+		return 0;
+	}
+	//check keypad
+	if (skeypad == L"normal")
+		keypad = BACKTYPE::NORMAL;
+	else if (sdisplay == L"windows")
+		keypad = BACKTYPE::WINDOWS;
+	else if (sdisplay == L"DX")
+		keypad = BACKTYPE::DX;
+	else if (sdisplay == L"opengl")
+		keypad = BACKTYPE::OPENGL;
+	else {
+		setlog(L"error sdisplay=%s", sdisplay.c_str());
+		return 0;
+	}
+	//check hwnd
 	if (!::IsWindow(_hwnd)) {
+		setlog(L"invalid window hwnd.");
 		ret = 0; _hwnd = 0;
 	}
 	else {
 		ret = 1;
-		_display = display;
-		_keypad = keypad; _mode = mode;
+		_mode = mode;
 		if (!_bkdisplay.Bind(_hwnd, mode))
 			return 0;
 		if (!_bkmouse.Bind(_hwnd, mouse))
@@ -36,7 +77,7 @@ long Background::Bind(int hwnd, int display, int mouse, int keypad, int mode) {
 long Background::UnBind() {
 	_hwnd = NULL;
 	_is_bind = 0;
-	_display = _keypad = _mode = 0;
+	_mode = 0;
 	_bkdisplay.UnBind();
 	_bkmouse.UnBind();
 	return 1;
