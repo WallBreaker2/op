86f3d67ce8dd161cbb766fb2b617de18e22d44ac
wallbreaker2 <784942619@qq.com>
2019-01-13 10:25:00 +0800

    重构部分代码，修复一些bug

--- PythonTest/PythonTest.py
+++ PythonTest/PythonTest.py
@@ -7,18 +7,21 @@ print("op ver:",op.Ver());
 hwnd=op.FindWindow("","HDRLighting");
 r=0;
 if hwnd:
-	r=op.BindWindow(hwnd,"dx","windows","windows",0);
-if r:
-	print("bind ok.");
-	r=op.Sleep(2000);
-	print("try screencap");
-	r=op.capture("dx_screen.bmp");
-	r,x,y=op.FindPic(0,0,800,600,"test.bmp",0.9);
-	print(r,x,y);
-	if r:
-		op.MoveTo(x,y);
+    r=op.BindWindow(hwnd,"dx","windows","windows",0);
+    if r:
+	    print("bind ok.");
+	    r=op.Sleep(2000);
+	    print("try screencap");
+	    r=op.capture("dx_screen.bmp");
+	    r,x,y=op.FindPic(0,0,800,600,"test.bmp",0.9);
+	    print(r,x,y);
+	    if r:
+		    op.MoveTo(x,y);
+    else:
+        print("inject false.");
 else:
-	print("inject false.");
+    print("invalid window.");
+
 op.UnBind();
 print("test end");
 
--- op/Bkdx.h
+++ op/Bkdx.h
@@ -8,22 +8,6 @@
 #include "Common.h"
 #include "ImageLoc.h"
 #include <boost/interprocess/sync/named_mutex.hpp> 
-long CreateSharedMemory(const std::string& name);
-
-long ReleaseSharedMemory(const std::string& name);
-
-//此函数做以下工作
-/*
-1.hook相关函数
-2.设置共享内存
-3.截图(hook)至共享内存
-*/
-DLL_API long SetDX9Hook(HWND hwnd);
-//恢复原状态，释放共享内存
-DLL_API long UnDX9Hook();
-
-
-
 
 using std::wstring;
 class Bkdx
@@ -31,13 +15,20 @@ class Bkdx
 public:
 	Bkdx();
 	~Bkdx();
+	//1
 	long Bind(HWND hwnd);
 	long UnBind();
+
+	long bind_init();
+	long bind_release();
 	//截图至文件
 	long capture(const std::wstring& file_name);
 	//图形定位
 	long FindPic(long x1, long y1, long x2, long y2, const std::wstring& files, double sim, long& x, long &y);
+
+
 private:
+	int _is_bind;
 	HWND _hwnd;
 	wstring _dllname;
 	long _width, _height;
@@ -45,6 +36,9 @@ private:
 	ImageLoc _imageloc;
 	//user mutex
 	boost::interprocess::named_mutex* _pmutex;
+	char _shared_res_name[256];
+	char _mutex_name[256];
+	byte* _image_data;
 };
 
 #endif
