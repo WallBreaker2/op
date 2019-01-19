4b7724658ae0c25eb336495c92ee3c184f1ffb42
wallbreaker2 <784942619@qq.com>
2019-01-19 22:50:54 +0800

    添加字库编辑

--- Tool/Tool.h
+++ Tool/Tool.h
@@ -6,6 +6,10 @@
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
+#include <QStringListModel>
+#include <QStandardItemModel>
+#include <QModelIndex>
+#include "ximage.h"
 class Tool : public QMainWindow
 {
 	Q_OBJECT
@@ -16,14 +20,21 @@ public:
 	void load_image();
 	void to_binary();
 	void hist();
-	void show_next_char();
 	void draw_line(const std::vector<int>&lines, QPainter& paint,int isy,QGroupBox*,Qt::GlobalColor cr=Qt::black);
+	void show_char(const QModelIndex& idx);
+	void load_dict();
+	void save_dict();
+	void add_word();
+	void edit_dict();
 private:
 	Ui::ToolClass ui;
 	cv::Mat _src;
 	cv::Mat _gray;
 	cv::Mat _binary;
-	std::vector<cv::Mat>_chars;
-	int _char_idx;
-	std::vector<int> _ys, _xs;
+	int _is_edit;
+	QStringListModel* _model;
+	QStandardItemModel* _itemmodel;
+	Dict _dict;
+	Dict _dict_tmp;
+	word_t _curr_word;
 };
